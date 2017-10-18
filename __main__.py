ec7a0e0223d8cde6bb8f644b72b611d693795f7f
Oliver Sabiniarz <sabiniao@informatik.hu-berlin.de>
2017-10-18 16:18:47 +0200

    rewritten, object detection works really neatly now

--- __main__.py
+++ __main__.py
@@ -1,60 +1,87 @@
+import threading
 import cv2
 import numpy as np
-import threading
-import matplotlib.pyplot as plt
-from typing import List, Optional
+from robodetect.Constants import ESC, RED_BGR
+from robodetect.Detection import calculate_perspective_warp
+from robodetect.Detection import detect_moving_object_on_raw_image
+from robodetect.Detection import warp_image
+from robodetect.Detection import get_image_coordinates_of_objects
 
 
-stream = cv2.VideoCapture(0)
-bg_subtractor = cv2.createBackgroundSubtractorMOG2(history=3600,
-                                                   varThreshold=400.0)
-dst_width= 1000
+# globals
+stream = cv2.VideoCapture('playground/robo_on_paper.mov')
+bg_subtractor = cv2.createBackgroundSubtractorMOG2(
+    history=1000, varThreshold=400.0
+)
+close_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+dst_width = 1000
 dst_height = 1414
+M = np.ones((3,3))
+_4_src_points = []
 
 class GetSrcPoints(threading.Thread):
     # TODO: integrate plt.show
     def __init__(self):
         threading.Thread.__init__(self)
-        self._4_src_points = []
+        global _4_src_points
+        _4_src_points = []
 
+    # uncomment to provide input points via CL
+    """
     def run(self):
         print('Provide 4 points from source image')
         for i in range(4):
             print('Point {}:'.format(i))
             x = input('x ')
             y = input('y ')
-            self._4_src_points.append([x, y])
+            _4_src_points.append([x, y])
+    """
+    def run(self):
+        global _4_src_points
+        _4_src_points = [[448,393], [667,389], [731,565], [423,571]]
 
-
-def calculate_perspective_warp(_4_src_points: Optional[List[List], np.ndarray],
-                               _4_dst_points: Optional[List[List], np.ndarray]):
-    M = cv2.getPerspectiveTransform(np.float32(_4_src_points),
-                                    np.float32(_4_dst_points))
-    return M
 
 def main():
-
     # init
-    M = calculate_perspective_warp()
     getSrcPoints = GetSrcPoints()
+    getSrcPoints.start()
+    # TODO: code for showing frame
+    getSrcPoints.join()
+
+    warp_matrix = calculate_perspective_warp(
+        _4_src_points,
+        [[0,0],[dst_width,0],[dst_width, dst_height],[0,dst_height]]
+    )
 
     ok, frame = stream.read()
-
-
     # main loop, reading and processing image from stream
-    while True:
+    while stream.isOpened():
         prev_frame = frame
         ok, frame = stream.read()
         if not ok:
             frame = prev_frame
 
-        warped_frame = cv2.warpPerspective(frame, M, (dst_width, dst_height))
-        fg_frame = bg_subtractor.apply(warped_frame)
+        warped_frame = warp_image(frame, dst_width, dst_height, warp_matrix)
 
+        objects = detect_moving_object_on_raw_image(
+            frame, dst_width, dst_height,
+            warp_matrix, bg_subtractor, close_kernel,
+            area_threshold=17000.0
+        )
 
+        object_coordinates = get_image_coordinates_of_objects(objects)
 
+        for position in object_coordinates:
+            cv2.drawMarker(warped_frame, position, RED_BGR)
+        # cv2.drawContours(warped_frame, objects, -1, RED_BGR, 2)
+        # show result
+        cv2.imshow('processed', warped_frame)
 
+        if cv2.waitKey(30) & 0xff == ESC:
+            break
 
 
 if __name__ == '__main__':
     main()
+    stream.release()
+    cv2.destroyAllWindows()
